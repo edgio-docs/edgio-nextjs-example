2eee67c4b0faf8c64d212f12f8667b81c61d3645
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-10-03 03:13:05 +0530

    Header changes

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 import { Prefetch } from '@layer0/react'
-import styles from '../styles/Header.module.css'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { getCategories } from '../lib/cms'
+import { getCategories } from '@/lib/cms'
 
 export default function Header() {
   const [categories, setCategories] = useState()
@@ -20,7 +19,7 @@ export default function Header() {
   }, [])
 
   useEffect(() => {
-    router.events.on('routeChangeComplete', url => {
+    router.events.on('routeChangeComplete', (url) => {
       if (categories) {
         setActiveTab(categories.findIndex(({ href }) => href === url))
       }
@@ -31,20 +30,17 @@ export default function Header() {
 
   return (
     <>
-      <header className="bg-white rounded-lg p-2 justify-center">
-        <div className={`container ${styles.logoContainer}`}>
-          <Link href="/" passHref>
-            <a>
-              <img src="/layer0-icon.svg" alt="Layer0 Logo" />
-              <div className="text-center text-gray-700">Next.js Example</div>
-            </a>
-          </Link>
-        </div>
-        <div className={`md:flex ${styles.container}`}>
-          <ul>
+      <header className="bg-white pt-2 flex flex-col items-center">
+        <Link href="/">
+          <a>
+            <img width="200px" height="55.59" src="/layer0-icon.svg" alt="Layer0 Logo" />
+            <div className="text-center text-gray-700">Next.js Example</div>
+          </a>
+        </Link>
+        <div className="flex flex-col items-center w-full border-b-[1px] border-[#eaeaea]">
+          <div className={`py-4 w-2/3 md:flex flex flex-row justify-between`}>
             {categories.map(({ categoryName, href }, i) => {
               const prefetchProps = {}
-
               if (process.browser) {
                 // prefetch URL needs to include the `name` param otherwise it will be a browser miss
                 prefetchProps.url = `/_next/data/${__NEXT_DATA__.buildId}${href}.json?name=${
@@ -53,16 +49,19 @@ export default function Header() {
               }
 
               return (
-                <li key={categoryName} className={activeTab === i ? styles.active : null}>
+                <div
+                  key={categoryName}
+                  className={activeTab === i ? 'border-b-[3px] border-[#ff0000]' : null}
+                >
                   <Link href={href} passHref>
                     <Prefetch {...prefetchProps}>
                       <a>{categoryName}</a>
                     </Prefetch>
                   </Link>
-                </li>
+                </div>
               )
             })}
-          </ul>
+          </div>
         </div>
       </header>
     </>
