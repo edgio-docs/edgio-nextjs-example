ffaa8ee6ae5fa7d42831c4e57b67624bbafaa4b3
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-10-03 03:12:06 +0530

    Config Changes

--- layer0.config.js
+++ layer0.config.js
@@ -1,13 +1,5 @@
-// This file was automatically added by layer0 deploy.
-// You should commit this file to source control.
-require('dotenv').config()
-const { join } = require('path')
-
 module.exports = {
   connector: '@layer0/next',
-  includeFiles: {
-    [join('.next', 'BUILD_ID')]: true,
-  },
   backends: {
     api: {
       domainOrIp: 'layer0-docs-layer0-examples-api-default.layer0.link',
