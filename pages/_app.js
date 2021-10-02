7d8a6561fd062d117ce4b493362eac7670e21dd5
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-10-03 03:14:38 +0530

    remove loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,14 @@
-import '../styles/globals.css'
-import '../styles/tailwind.css'
-import Header from '../components/Header'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import LoadingIndicator from '../components/LoadingIndicator'
+import '@/styles/globals.css'
+import Header from '@/components/Header'
+import { useEffect } from 'react'
 import { prefetch } from '@layer0/prefetch/window/prefetch'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
-
   useEffect(() => {
-    router.events.on('routeChangeStart', () => setLoading(true))
-    router.events.on('routeChangeComplete', () => setLoading(false))
-
     // register a listener for SW messages to prefetch images from the PLP API responses
     const { serviceWorker } = navigator
     if (serviceWorker) {
-      serviceWorker.addEventListener('message', event => {
+      serviceWorker.addEventListener('message', (event) => {
         if (event.data.action === 'prefetch') {
           prefetch(event.data.url, event.data.as, event.data.options)
         }
@@ -27,7 +18,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      {loading && <LoadingIndicator />}
       <Header />
       <Component {...pageProps} />
     </>
