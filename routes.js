e5ba8086cf94a331fbbaa5ef645a97656025cd07
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-27 23:54:31 +0530

    Add prerender requests

--- routes.js
+++ routes.js
@@ -1,5 +1,36 @@
-const { Router } = require('@layer0/core/router')
-const { nextRoutes } = require('@layer0/next')
+// Determine the buildId from the build of Next.js app
+let buildId = undefined
+import { join } from 'path'
+import { existsSync, readFileSync } from 'fs'
+const buildIdPath = join(process.cwd(), '.next', 'BUILD_ID')
+
+if (existsSync(buildIdPath)) {
+  buildId = readFileSync(buildIdPath, 'utf8')
+}
+
+// Import libraries to fetch and create prerender and Layer0 routes
+import fetch from 'node-fetch'
+import { nextRoutes } from '@layer0/next'
+import { Router } from '@layer0/core/router'
+
+// Function definition referred from node_modules/@layer0/next/client.js
+const createNextDataURL = (params) => {
+  if (buildId) {
+    let { href, routeParams = {} } = params
+    if (href.endsWith('/')) {
+      href += 'index'
+    }
+    let qs = ''
+    if (routeParams) {
+      const keys = Object.keys(routeParams)
+      if (keys.length) {
+        qs = '?' + keys.map((key) => `${key}=${encodeURIComponent(routeParams[key])}`).join('&')
+      }
+    }
+    return `/_next/data/${buildId}${href}.json${qs}`
+  }
+  return undefined
+}
 
 const ASSET_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
   // Remove the cache-control header coming in from the Next.js app, this is to
@@ -44,7 +75,32 @@ const NEXT_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
   })
 }
 
-const warmOnDeploymentCachePaths = ['/', '/images/grey.png']
+// Function to create all the paths to prerender
+async function getPathsToPrerender() {
+  const prerenderPaths = ['/']
+  // Get all the slugs for the product pages
+  const respCall = await fetch('https://layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link/products/all')
+  if (respCall.ok) {
+    const resp = await respCall.json()
+    // For each product, derive the non slash path and create the next data url to populate on the edge
+    resp.forEach((i) => {
+      const nonSlashPath = i.path.replace(/\//g, '')
+      prerenderPaths.push(
+        // Using createNextDataURL, make it easier to generate Next.js data json(s)
+        createNextDataURL({
+          href: `/product/${nonSlashPath}`,
+          routeParams: {
+            name: nonSlashPath,
+          },
+        })
+      )
+    })
+  } else {
+    console.log('The fetch call failed, no paths to populate cache with.')
+  }
+  // Finally, return all the paths with the intended mapping to Layer0 prerender syntax
+  return prerenderPaths.filter((i) => i).map((i) => ({ path: i }))
+}
 
 module.exports = new Router()
   // Pre-render the static home page
@@ -52,7 +108,7 @@ module.exports = new Router()
   // the set of links are visited to warm the cache
   // for future visits (expected to be the first view for real users)
   // More on static prerendering: https://docs.layer0.co/guides/static_prerendering
-  .prerender(warmOnDeploymentCachePaths.map((i) => ({ path: i })))
+  .prerender(getPathsToPrerender)
   // Serve the compiled service worker with Layer0 prefetcher working
   .match('/service-worker.js', ({ serviceWorker }) => {
     return serviceWorker('.next/static/service-worker.js')
