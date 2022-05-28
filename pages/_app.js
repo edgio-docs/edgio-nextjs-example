83592e51d995270f7b99f920decffe363345d15d
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-28 10:31:13 +0530

    Abstract Layer0 RUM

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,10 @@
+import Layer0RUM from 'rum'
 import '@/styles/globals.css'
-import { Metrics } from '@layer0/rum'
-import Router from '@layer0/rum/Router'
 import Navbar from '@/components/Navbar'
 
-// Implementing Real Time User Monitoring (Core Web Vitals)
-// https://docs.layer0.co/guides/core_web_vitals#npm-or-yarn
 // Include the RUM Analytics in the production build only
 if (process.env.NODE_ENV === 'production') {
-  new Metrics({
-    // Set this TOKEN as an environment variable at Layer0 Console
-    // More on creating env variables: https://docs.layer0.co/guides/environments#creating-environment-variables
-    token: process.env.RUM_TOKEN,
-    router: new Router()
-      .match('/', ({ setPageLabel }) => setPageLabel('home'))
-      .match('/search', ({ setPageLabel }) => setPageLabel('search'))
-      .match('/search/:id', ({ setPageLabel }) => setPageLabel('search'))
-      .match('/products/:id', ({ setPageLabel }) => setPageLabel('products/:id')),
-  }).collect()
+  Layer0RUM(process.env.RUM_TOKEN)
 }
 
 const MyApp = ({ Component, pageProps }) => {
---
+++ rum.js
@@ -0,0 +1,17 @@
+import { Metrics } from '@layer0/rum'
+import Router from '@layer0/rum/Router'
+
+// Implementing Real Time User Monitoring (Core Web Vitals)
+// https://docs.layer0.co/guides/core_web_vitals#npm-or-yarn
+export default function Layer0RUM(token) {
+  new Metrics({
+    // Set this TOKEN as an environment variable at Layer0 Console
+    // More on creating env variables: https://docs.layer0.co/guides/environments#creating-environment-variables
+    token,
+    router: new Router()
+      .match('/', ({ setPageLabel }) => setPageLabel('home'))
+      .match('/search', ({ setPageLabel }) => setPageLabel('search'))
+      .match('/search/:id', ({ setPageLabel }) => setPageLabel('search'))
+      .match('/products/:id', ({ setPageLabel }) => setPageLabel('products/:id')),
+  }).collect()
+}
