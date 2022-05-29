e470a19dbd96e734a17ce0d13dd1f8e3b70bed24
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-29 14:36:18 +0530

    improve cache documentation

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -21,7 +21,8 @@ new Prefetcher({
   ],
 })
   .route()
-  // Cache the images coming from https://opt.moovweb.net
+  // Cache the images coming from any route (including
+  // cross-origin) assets that contains `.link` in it
   // Read more on caching cross-origin requests at
   // https://docs.layer0.co/docs/api/prefetch/classes/_sw_prefetcher_.prefetcher.html#cache
   .cache(/^https:\/\/(.*?)\.link\/.*/)
