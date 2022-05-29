1175bd614363ec5f749e0a9fff322e452f1463fa
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-29 13:26:59 +0530

    Cache cross-origin images

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -19,8 +19,9 @@ new Prefetcher({
       },
     ]),
   ],
-}).route()
-// Cache the images coming from https://opt.moovweb.net
-// Read more on caching cross-origin requests at
-// https://docs.layer0.co/docs/api/prefetch/classes/_sw_prefetcher_.prefetcher.html#cache
-// .cache(/^https:\/\/opt\.moovweb\.net\/.*/)
+})
+  .route()
+  // Cache the images coming from https://opt.moovweb.net
+  // Read more on caching cross-origin requests at
+  // https://docs.layer0.co/docs/api/prefetch/classes/_sw_prefetcher_.prefetcher.html#cache
+  .cache(/^https:\/\/(.*?)\.link\/.*/)
