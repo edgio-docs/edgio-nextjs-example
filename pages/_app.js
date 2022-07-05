6b379798d348196f17b5c15fb08b14832b2b3adf
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-05 15:50:24 +0530

    Enable prefetcher in window

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import Layer0RUM from 'rum'
 import '@/styles/globals.css'
 import { useEffect } from 'react'
 import Navbar from '@/components/Navbar'
-import { relativizeURL } from '@/components/Shimmer'
-import { prefetch } from '@layer0/prefetch/window/prefetch'
+import { install } from '@layer0/prefetch/window'
 
 // Include the RUM Analytics in the production build only
 if (process.env.NODE_ENV === 'production') {
@@ -12,15 +11,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
-    // Register a listener for SW messages to prefetch images
-    const { serviceWorker } = navigator
-    if (serviceWorker) {
-      serviceWorker.addEventListener('message', (event) => {
-        if (event.data.action === 'prefetch') {
-          prefetch(relativizeURL(event.data.url), event.data.as, event.data.options)
-        }
-      })
-    }
+    // Enable service worker inside the window
+    install()
   }, [])
 
   return (
