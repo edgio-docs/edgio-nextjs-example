3003853404171af3045ed2f096cb62d3a5e09761
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-05 15:50:54 +0530

    Better showcase use of json query

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,3 +1,4 @@
+import { relativizeURL } from '@/lib/helper'
 import { Prefetcher } from '@layer0/prefetch/sw'
 import { precacheAndRoute } from 'workbox-precaching'
 import { skipWaiting, clientsClaim } from 'workbox-core'
@@ -12,8 +13,15 @@ new Prefetcher({
     new DeepFetchPlugin([
       // Query the PDP API response for images to prefetch
       // Prefetch login is handled in pages/_app.js
+      // Relativize the absolute link using json-query helpers
+      // relativize function is applied on the array returned by the selector
       {
-        jsonQuery: 'pageProps.data.images.url',
+        jsonQuery: 'pageProps.data.images.url:relativize',
+        jsonQueryOptions: {
+          locals: {
+            relativize: (input) => input.filter((i) => i).map((i) => relativizeURL(i)),
+          },
+        },
         maxMatches: 6,
         as: 'image',
       },
