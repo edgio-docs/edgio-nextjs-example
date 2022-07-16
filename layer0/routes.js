f0adff05c16877817524fe409764981c2aef3073
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-16 15:55:26 +0530

    Abstract in cache.js

--- layer0/routes.js
+++ layer0/routes.js
@@ -1,65 +1,7 @@
 import { Router } from '@layer0/core/router'
 import { BACKENDS } from '@layer0/core/constants'
 import getPathsToPrerender from '@/layer0/prerenderRequests'
-
-const ASSET_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
-  // Remove the cache-control header coming in from the Next.js app,
-  // and remove the set-cookie header coming in from the Next.js app,
-  // this is to ensure that the response is cacheable
-  removeUpstreamResponseHeader('set-cookie')
-  removeUpstreamResponseHeader('cache-control')
-  // Set the caching values
-  cache({
-    edge: {
-      // Save the response(s) [whether stale or updated] in the edge POP for a year
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-    },
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Layer0 service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-    },
-  })
-}
-
-const NEXT_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
-  // Remove the cache-control header coming in from the Next.js app,
-  // this is to ensure that the response is cacheable
-  removeUpstreamResponseHeader('cache-control')
-  // Set the caching values
-  cache({
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Layer0 service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-    },
-    edge: {
-      // Save the response(s) [whether stale or updated] in the edge POP for a year
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-      // Keep revalidating data per day, i.e. looking for content changes from the Next.js app
-      // and update the response in edge
-      // More on: https://web.dev/stale-while-revalidate
-      // and https://docs.layer0.co/guides/caching#achieving-100-cache-hit-rates
-      staleWhileRevalidateSeconds: 60 * 60 * 24,
-    },
-  })
-}
-
-const API_CACHE_HANDLER = ({ cache, proxy }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60,
-      // Cache responses even if they contain cache-control: private header
-      // https://docs.layer0.co/guides/caching#private
-      // https://docs.layer0.co/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
-      forcePrivateCaching: true,
-    },
-    browser: false,
-  })
-  proxy('api', { path: ':path*' })
-}
+import { API_CACHE_HANDLER, ASSET_CACHE_HANDLER, NEXT_CACHE_HANDLER } from './cache'
 
 const router = new Router()
   .get(
