2df8e3e3672186eab48b0c976776ec0baf7b39d4
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-18 22:18:15 +0530

    Left side bar selected indicators

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import { Fragment } from 'react'
 import classNames from 'classnames'
+import { useRouter } from 'next/router'
+import { Fragment, useEffect, useState } from 'react'
 
 const listingItems = {
   'All Categories': [
@@ -24,6 +25,13 @@ const listingItems = {
 }
 
 const LeftSidebar = () => {
+  const router = useRouter()
+  const [pathWithoutQuery, setPathWithoutQuery] = useState(router.asPath)
+  useEffect(() => {
+    let temp = router.asPath
+    if (temp.includes('?')) setPathWithoutQuery(temp.substring(0, temp.indexOf('?')))
+    else setPathWithoutQuery(temp)
+  }, [router.asPath])
   return (
     <div className="flex w-full flex-col">
       {Object.keys(listingItems).map((item, index) => (
@@ -32,7 +40,15 @@ const LeftSidebar = () => {
           {listingItems[item].map((subItem) => (
             <Link passHref key={subItem.name} href={subItem.route}>
               <a>
-                <h3 className="text-md mt-2 font-light text-[#FFFFFF75]">{subItem.name}</h3>
+                <h3
+                  className={classNames(
+                    'text-md mt-2',
+                    { 'font-light text-[#FFFFFF75]': pathWithoutQuery !== subItem.route },
+                    { 'font-medium text-[#FFFFFF]': pathWithoutQuery === subItem.route }
+                  )}
+                >
+                  {subItem.name}
+                </h3>
               </a>
             </Link>
           ))}
