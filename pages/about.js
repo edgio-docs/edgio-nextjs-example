28dfeb77d44d8a712b928d7945443c794c978bfa
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-07-19 01:21:34 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -33,9 +33,10 @@ export async function getStaticProps() {
   return {
     props: {
       showcases: [
-        `Layer0's Built-In Support for Next.js 12.1`,
+        `Layer0's Compatbility with Next.js 12.2`,
         'Static Pre-rendering with Layer0, to warm cache as soon as the app goes to production (or the cache is cleared)',
         '(Free) Real Time User Monitoring service offered by Layer0',
+        '(Free) Layer0 Image Optimization Service',
         'Edge and Browser caching of SSR page(s)',
         'Prefetching with Layer0 Service Worker to speeden up transitions',
         'Proxying and Caching external API(s) for faster edge and browser responses',
