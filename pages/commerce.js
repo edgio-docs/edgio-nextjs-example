4c781612518e7329f0f024980511fd1f6f17de65
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-20 23:52:53 +0530

    Update commerce.js

--- pages/commerce.js
+++ pages/commerce.js
@@ -14,7 +14,7 @@ const Search = ({ data }) => {
         </div>
         <div className="flex flex-col items-start pt-5">
           <h2 className="text-[#FFFFFF75]">Showing {data.length} Results</h2>
-          <div className="sm:grd-cols-2 mt-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div className="sm:grid-cols-2 mt-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             {filterProducts(data, router.query.filter).map((i) => (
               <ProductPreview key={i.path} {...i} />
             ))}
