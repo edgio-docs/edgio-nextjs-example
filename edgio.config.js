a0e96010d56647317deb04b445269d1052d1e6c2
Tristan Lee <tristan.lee@moovweb.com>
2022-12-07 17:28:43 -0500

    Fix api url

--- edgio.config.js
+++ edgio.config.js
@@ -5,6 +5,15 @@ module.exports = {
   connector: '@edgio/next',
   routes: './edgio/routes.js',
   backends: {
+    // Define a domain or IP address to proxy as a backend
+    // More on: https://docs.edgio.co/guides/edgio_config#backends
+    api: {
+      domainOrIp: 'layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link',
+      hostHeader: 'layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link',
+      // Disable backend SSL certificate security check, read more on:
+      // https://docs.edgio.co/guides/edgio_config#:~:text=browser%20is%20used.-,disableCheckCert,-Boolean
+      disableCheckCert: true,
+    },
     // More on: https://docs.edgio.co/guides/image_optimization
     image: {
       domainOrIp: 'opt.moovweb.net',
